623fa70e717b090d57e8514f4ad36f07df2137ca
Christian Fredericksen <christian.fredericksen@yahoo.com>
2019-11-13 20:45:36 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
+require "pry"
+#we're going through each element in the array, square each element, and finally add that new element to a new array. new_array=[]
 def square_array(array)
-  # your code here
+  new_array=[]
+  counter = 0
+  while counter < array.size do
+    
+   new_array << array[counter]**2
+    counter += 1
+  end
+  new_array
 end
